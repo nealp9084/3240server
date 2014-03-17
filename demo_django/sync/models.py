1e3353a821bafeb37c048afd79728016f701b30f
Neal Patel <nealp9084@gmail.com>
2014-03-16 23:09:58 -0400

    Added a method for syncing files

--- demo_django/sync/models.py
+++ demo_django/sync/models.py
@@ -11,16 +11,16 @@ class File(models.Model):
   def __unicode__(self):
     return "[id=%d] %s's %s" % (self.id, self.owner.name, self.path)
 
-  def is_sync_needed(self, queried_timestamp):
+  def is_sync_needed(self, user_timestamp):
     """
     Determines whether a particular file needs to be synced using the timestamp
     value from the user's file.
     """
-    if self.last_modified < queried_timestamp:
+    if self.last_modified < user_timestamp:
       # The file on the user's filesystem is newer than the one stored online
       # must sync: replace the file on the server
       return True
-    elif self.last_modified == queried_timestamp:
+    elif self.last_modified == user_timestamp:
       # The file on the user's filesystem has the same timestamp as the one
       # stored online
       # do not sync
@@ -29,3 +29,12 @@ class File(models.Model):
       # The file on the user's filesystem is older than the one stored online
       # must sync: replace the file on the user's filesystem
       return True
+
+  def sync(self, user_timestamp, user_data):
+    """
+    Updates the contents of a particular file on the server.
+    """
+    assert self.is_sync_needed(user_timestamp)
+
+    self.last_modified = user_timestamp
+    self.data = user_data
