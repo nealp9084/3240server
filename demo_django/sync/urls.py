ad898a3037958ce9c2eb816ad5c7472be19e054b
Neal Patel <nealp9084@gmail.com>
2014-03-23 22:09:35 -0400

    added routes for the File views

--- demo_django/sync/urls.py
+++ demo_django/sync/urls.py
@@ -4,4 +4,7 @@ from sync import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'^create_server_file/$', views.create_server_file, name='create_server_file'),
+    url(r'^(?P<file_id>\d+)/update_file/$', views.update_file, name='update_file'),
+    url(r'^(?P<file_id>\d+)/serve_file/$', views.serve_file, name='serve_file'),
 )
