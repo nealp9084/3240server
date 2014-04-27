7994fad6117803aae066454ea5ba6b9d858e4018
morpheus <aqy8je@virginia.edu>
2014-04-27 18:24:06 -0400

    Ghost folder.

--- demo_django/sync/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from sync.models import File, History
-
-# Register your models here.
-admin.site.register(File)
-admin.site.register(History)
--- demo_django/tokens/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from tokens.models import *
-
-# Register your models here.
-admin.site.register(Token)
