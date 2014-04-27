a0f2a5e9f1503db5903434cf7f155a1d434b24cc
morpheus <aqy8je@virginia.edu>
2014-04-27 13:50:55 -0400

    Admin only index for Files written.

--- demo_django/sync/views.py
+++ demo_django/sync/views.py
@@ -18,13 +18,28 @@ def index(request):
     if not current_user:
       return HttpResponseForbidden()
 
-    if current_user.is_admin:
-      objs = [x.to_dict() for x in list(File.objects.all())]
-    else:
-      objs = [x.to_dict() for x in list(File.objects.all().filter(owner=current_user))]
+    objs = [x.to_dict() for x in list(File.objects.all().filter(owner=current_user))]
 
     json_data = json.dumps(objs)
     return HttpResponse(json_data)
+  else:
+    return HttpResponseNotAllowed(['GET'])
+
+def admin_index(request):
+  if request.method == 'GET':
+    param_secret = request.GET['token']
+
+    # get the current user via the access token
+    current_user = Token.get_current_user(param_secret)
+    if not current_user:
+      return HttpResponseForbidden()
+
+    if current_user.is_admin:
+      objs = [x.to_dict() for x in list(File.objects.all())]
+      json_data = json.dumps(objs)
+      return HttpResponse(json_data)
+    else:
+      return HttpResponseForbidden()
   else:
     return HttpResponseNotAllowed(['GET'])
 
